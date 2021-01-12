97b223f2c5554a9090b503724542fa76b506e029
sakshipink <44083326+sakshipink@users.noreply.github.com>
2021-01-12 01:53:17 -0500

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,9 +1,10 @@
 <?php $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
+$purpose = $_POST['purpose'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "business@praan.io";
-$subject = "Contact Form";
+$recipient = "ssdeshp8@ncsu.edu";
+$subject = "Contact Form : $purpose";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 header("Location: Contact___1.html");
